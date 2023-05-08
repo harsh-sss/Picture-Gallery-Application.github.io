f1a6d82bef747b94181121ec2ee8cf763bb1c268
Harsh <harshvardhan.jha@somaiya.edu>
2023-05-08 05:54:58 +0530

    updated description

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const grid = document.querySelector('.grid')
-const input = document.getElementById('input')
-const submitBtn = document.getElementById('submit')
+const grid = document.querySelector('.grid');
+const input = document.getElementById('input');
+const submitBtn = document.getElementById('submit');
 
 const macyInstance = Macy({
   container: grid,
@@ -11,85 +11,93 @@ const macyInstance = Macy({
     600: 2,
     400: 1,
   },
-})
+});
 
-const key = 'Gj812_xvgyKHIXLLsaP29BadzLP59cvQm6g4YNCZLsM'
+const key = 'Gj812_xvgyKHIXLLsaP29BadzLP59cvQm6g4YNCZLsM';
 
-const API_URL = 'https://api.unsplash.com'
+const API_URL = 'https://api.unsplash.com';
 
 const fixStartUpBug = () => {
   macyInstance.runOnImageLoad(function () {
-    macyInstance.recalculate(true, true)
-    var evt = document.createEvent('UIEvents')
-    evt.initUIEvent('resize', true, false, window, 0)
-    window.dispatchEvent(evt)
-  }, true)
-}
+    macyInstance.recalculate(true, true);
+    var evt = document.createEvent('UIEvents');
+    evt.initUIEvent('resize', true, false, window, 0);
+    window.dispatchEvent(evt);
+  }, true);
+};
 
-const addImagesInDom = images => {
-  images.forEach(image => {
-    const container = document.createElement('div')
+const addImagesInDom = (images) => {
+  images.forEach((image) => {
+    const container = document.createElement('div');
 
-    const img = document.createElement('img')
-    img.src = image.urls.regular
-    container.append(img)
+    const img = document.createElement('img');
+    img.src = image.urls.regular;
+    img.alt = image.alt_description;
+    container.append(img);
 
-    const author = document.createElement('p')
-    author.textContent = `By ${image.user.name}`
-    container.append(author)
+    const author = document.createElement('p');
+    author.classList.add('author');
+    author.textContent = `author: ${image.user.name}`;
 
-    const link = document.createElement('a')
-    link.href = image.links.html
-    link.target = '_blank'
-    link.textContent = 'View on Unsplash'
-    container.append(link)
+    const description = document.createElement('p');
+    description.classList.add('description');
+    description.textContent = image.alt_description;
 
-    grid.append(container)
-  })
-}
+    const link = document.createElement('a');
+    link.href = image.links.html;
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+    link.textContent = 'View on Unsplash';
 
-// const intializeImages = async () => {
-//   let { data: images } = await axios.get(
-//     `${API_URL}/photos/?client_id=${key}&per_page=50`
-//   )
-
-//   addImagesInDom(images)
-
-//   fixStartUpBug()
-// }
-
-// intializeImages()
-
-const searchImages = async query => {
+    container.append(author, description, link);
+    grid.append(container);
+  });
+};
+const searchImages = async (query) => {
   let {
     data: { results: images },
   } = await axios.get(
     `${API_URL}/search/photos/?client_id=${key}&query=${query}&per_page=50`
-  )
+  );
 
-  return images
-}
+  images = images.map((image) => {
+    return {
+      urls: {
+        regular: image.urls.regular,
+      },
+      alt_description: image.alt_description,
+      user: {
+        name: image.user.name,
+      },
+      links: {
+        html: image.links.html,
+      },
+    };
+  });
 
-const removeAllChild = parent => {
+  return images;
+};
+
+const removeAllChild = (parent) => {
   while (parent.firstChild) {
-    parent.removeChild(parent.firstChild)
+    parent.removeChild(parent.firstChild);
   }
-}
+};
 
-const handleSubmit = async event => {
-  event.preventDefault()
+const handleSubmit = async (event) => {
+  event.preventDefault();
 
-  const query = input.value
+  const query = input.value;
 
-  if (!query) return false
+  if (!query) return false;
 
-  const images = await searchImages(query)
+  const images = await searchImages(query);
 
-  removeAllChild(grid)
+  removeAllChild(grid);
 
-  addImagesInDom(images)
+  addImagesInDom(images);
 
-  fixStartUpBug()
-}
+  fixStartUpBug();
+};
 
-submitBtn.addEventListener('click', handleSubmit)
+submitBtn.addEventListener('click', handleSubmit);
